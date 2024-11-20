e6d1be55189bd749cdf286dcab2b1be9303fc93a
Alejandra Chavez Zamorano <alechazamo.2@gmail.com>
2024-11-19 18:05:49 -0600

    Add checkboxes for interaction

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
-# Cargar el conjunto de datos
-df = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
-# Título de la aplicación
-st.title("Análisis Exploratorio de Vehículos")
+show_histogram = st.checkbox('Mostrar histograma interactivo')
 
-# Crear y mostrar un histograma
-st.subheader("Histograma del odómetro")
-fig_hist = px.histogram(df, x="odometer")
-st.plotly_chart(fig_hist)
+if show_histogram:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer", title="Histograma de Kilometraje (Odometer)")
+    st.plotly_chart(fig, use_container_width=True)
 
-# Crear y mostrar un gráfico de dispersión
-st.subheader("Gráfico de Dispersión: Odómetro vs Precio")
-fig_scatter = px.scatter(df, x="odometer", y="price")
-st.plotly_chart(fig_scatter)
+show_scatter = st.checkbox('Mostrar gráfico de dispersión')
+
+if show_scatter:
+    st.write('Creación de un gráfico de dispersión entre el kilometraje y el precio')
+    fig = px.scatter(car_data, x="odometer", y="price", title="Gráfico de Dispersión: Kilometraje vs Precio")
+    st.plotly_chart(fig, use_container_width=True)
+
+
